6f521072073fa280383a9161151689a6dffd0fc1
Md. Jafrin Hossin <kingjaaaf@gmail.com>
2023-02-13 03:03:44 +0600

    delete added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import data from './data';
 import List from './List';
 function App() {
 
-  const [people, setPeople] = [data]
+  const [people, setPeople] = useState(data)
 
 
   return <main>
     <section className='container'>
     <h3> {people.length} Birthday Today</h3>
     <List people = {people} />
-    <button onClick={console.log("YOU clicked")}> Clear All</button>
+    <button onClick={
+      () => setPeople([])
+    }> Clear All </button>
     </section>
   </main>;
 }
